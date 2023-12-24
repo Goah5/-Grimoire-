db3d9faf2b2c369485c25d2a4770e3dbd8ce1d76
Goah5 <zogar7777@gmail.com>
2023-12-25 03:45:47 +0600

    v1.3

---
+++ mine.py
@@ -0,0 +1,84 @@
+import os
+from sys import argv
+
+
+def get_page_number_list() -> list[int]:
+    l = list(map((lambda x: x.split()[0]), os.listdir()))
+    ll = []
+    for i in l:
+        if not i.isdigit():
+            i = i[:i.find('.')]
+        if i.isdigit():
+            ll.append(int(i))
+    return ll
+
+
+def get_next_page_number() -> int:
+    return max(get_page_number_list()+[0,]) + 1
+
+
+def create_next_page(flag: bool = False) -> None:
+    temp = get_next_page_number()
+
+    if flag:
+        os.mkdir(f"{temp}")
+        # print(f"Gen dir {temp}")
+    else:
+        with open(f"{temp}.py", "w") as f:
+            pass
+        # print(f"Gen py {temp}.py")
+
+
+def display_commands_list(*arg) -> None:
+    global Comands_list
+    for i in Comands_list.keys():
+        print(f'{i}: {Comands_list[i][1]}')
+
+
+def f0(*arg) -> None:
+    quit()
+
+def f1(*arg) -> None:
+    if arg:
+        for i in range(int(arg[0])):
+            create_next_page(False)
+    else:
+        create_next_page(False)
+
+
+def f2(*arg) -> None:
+    if arg:
+        for i in range(int(arg[0])):
+            create_next_page(True)
+    else:
+        create_next_page(True)
+
+
+def main():
+    global Comands_list
+    display_commands_list()
+
+    while 1:
+        is_correct = False
+        while not is_correct:
+            inp = input("Введите Команду> ").split()
+            if inp != []:
+                is_correct = inp[0] in Comands_list
+
+        Comands_list[inp.pop(0)][0](*inp)
+
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+Comands_list = {"0": [f0, "Выход"],
+                "1": [display_commands_list, 'Спрафка'],
+                "2": [f1, 'Добавить Файл'],
+                "3": [f2, 'Добавить Папку']}
+
+
+if len(argv) > 1:
+    for i in argv[1:]:
+        if i in Comands_list.keys():
+            Comands_list[i][0]()
+
+if __name__ == "__main__":
+    main()
